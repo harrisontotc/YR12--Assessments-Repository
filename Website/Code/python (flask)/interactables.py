5823df74512214b917f3e75ff7bf580926697f3c
harrisontotc <harrisont@stu.otc.school.nz>
2025-04-30 15:12:21 +1200

    created practice database for website

--- Website/Code/python (flask)/interactables.py
+++ Website/Code/python (flask)/interactables.py
@@ -1,2 +1,14 @@
-import flask
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    #home page
+    return "Welcome to the home page!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
 
