6c2bf6b0bed6d941d02471f90e7830ea3f50b5a9
harrisontotc <harrisont@stu.otc.school.nz>
2025-04-30 15:50:19 +1200

    changed the website flask file name; linked database; added a handful of queries

---
+++ Website/Code/python (flask)/websiteFlask.py
@@ -0,0 +1,49 @@
+from flask import Flask, g
+import sqlite3
+import os
+
+DATABASE = r"G:\My Drive\Year 12 Programming Assessment Files\YR12--Assessments-Repository\Website\Code\database\database.db"
+
+app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+@app.route('/')
+def home():
+    #home page
+    sql = """SELECT Bikes.BikeID,Makers.Name,Bikes.Model,Bikes.ImagURL
+             FROM Bikes
+             JOIN Makers ON Makers.MakerID=Bikes.MakerID;"""
+    results = query_db(sql)
+    return str(results)
+
+@app.route("/bike/<int:id>")
+def bike(id):
+    sql = """SELECT * FROM Bikes 
+             JOIN Makers ON Makers.MakerID = Bikes.MakerID
+             WHERE Bikes.BikeID = ?;"""
+    result = query_db(sql, (id,), True)
+    return str(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
